96ffc205f084ea1e49a6647a4faef583841483f2
Michael De Luca <michael.deluca@circle-free.com>
2021-10-20 14:00:27 -0400

    feat: MapleProxied

---
+++ contracts/MapleProxied.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.7;
+
+import { Proxied } from "../modules/proxy-factory/contracts/Proxied.sol";
+
+/// @title MapleProxied is just Proxied.
+contract MapleProxied is Proxied {}
