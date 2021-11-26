a06355aafe34413df278ac442ed82e557113721e
Michael De Luca <michael.deluca@circle-free.com>
2021-11-26 10:28:18 -0500

    chore: PR fix

--- contracts/MapleProxyFactory.sol
+++ contracts/MapleProxyFactory.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.7;
 
-import { IProxied } from "../modules/proxy-factory/contracts/interfaces/IProxied.sol";
-
+import { IProxied }     from "../modules/proxy-factory/contracts/interfaces/IProxied.sol";
 import { ProxyFactory } from "../modules/proxy-factory/contracts/ProxyFactory.sol";
 
 import { IMapleProxyFactory } from "./interfaces/IMapleProxyFactory.sol";
