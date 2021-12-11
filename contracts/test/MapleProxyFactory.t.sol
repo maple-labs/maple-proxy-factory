b339bcdc0e8bd80d443186f44b5a2b774efc3cc4
Lucas Manuel <lucas@maple.finance>
2021-12-11 18:01:56 -0500

    fix: lock in solidity pragmas (#17)

--- contracts/test/MapleProxyFactory.t.sol
+++ contracts/test/MapleProxyFactory.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity ^0.8.7;
+pragma solidity 0.8.7;
 
 import { TestUtils }                            from "../../modules/contract-test-utils/contracts/test.sol";
 import { MockInitializerV1, MockInitializerV2 } from "../../modules/proxy-factory/contracts/test/mocks/Mocks.sol";
